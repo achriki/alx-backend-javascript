173aa31a54b8dace3c48f19391736660eb6c0819
Adam chriki <adam.chkp@gmail.com>
2024-06-03 23:57:04 +0100

    push tasks 0 and 1 for data manip

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+const getListStudentIds = (listObjs)=>{
+    let newList = [];
+    if (!(listObjs instanceof Array)){
+        return listObjs;
+    }
+    newList = listObjs.map((stds) => stds.id);
+    return newList;
+}
+
+export default getListStudentIds;
