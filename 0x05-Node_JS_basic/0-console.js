03d101e6d17cfedfb152db46bef75c3ff938ed59
Adam chriki <adam.chkp@gmail.com>
2024-07-24 00:38:20 +0100

    nodeJS Basics tasks

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,12 @@
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+// Arrow function
+const displayMessage = (input) => {
+    console.log(input);
+}
+
+// Default module export.
+module.exports = displayMessage;
